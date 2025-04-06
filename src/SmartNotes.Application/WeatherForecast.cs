e997fe705126d350a063c1b2e4072e0d2f3d7496
felipe d ferreira <felipedferreira11@gmail.com>
2025-04-06 01:43:56 -0400

    chore: enables CORS, adds better dir structure & patterns

--- src/SmartNotes.Application/WeatherForecast.cs
+++ src/SmartNotes.Application/WeatherForecast.cs
@@ -1,5 +1,5 @@
 ﻿namespace SmartNotes.Application;
 
-public class Class1
+public class WeatherForecast
 {
 }
---
+++ src/SmartNotes.WebApi/Endpoints/WeatherForecastEndpoints.cs
@@ -0,0 +1,41 @@
+using SmartNotes.Application;
+
+namespace SmartNotes.WebApi.Endpoints;
+
+public static class WeatherForecastEndpoints
+{
+    public static IEndpointRouteBuilder MapWeatherForecastEndpoints(this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/weatherforecast")
+            .WithTags("weatherforecast");
+
+        group.MapGet("/", () =>
+            {
+                var forecast = Enumerable.Range(1, 5).Select(index =>
+                        new WeatherForecast
+                        (
+                            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                            Random.Shared.Next(-20, 55),
+                            Summaries.ElementAt(Random.Shared.Next(Summaries.Count()))
+                        ))
+                    .ToArray();
+                return forecast;
+            })
+            .WithName("GetWeatherForecast");
+
+        return routes;
+    }
+    
+    private static readonly IEnumerable<string> Summaries = [
+        "Freezing",
+        "Bracing",
+        "Chilly",
+        "Cool",
+        "Mild",
+        "Warm",
+        "Balmy",
+        "Hot", 
+        "Sweltering",
+        "Scorching",
+    ];
+}

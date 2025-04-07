c16f619d0ca850bd6ec2548f1e3415dfff37ee63
felipe d ferreira <40349176+felipedferreira@users.noreply.github.com>
2025-04-06 23:24:20 -0400

    feat: containerizes application (#5)

--- src/SmartNotes.WebApi/Endpoints/WeatherForecastEndpoints.cs
+++ src/SmartNotes.WebApi/Endpoints/WeatherForecastEndpoints.cs
@@ -1,41 +1,43 @@
 using SmartNotes.Application.DataTransferObjects;
 
-namespace SmartNotes.WebApi.Endpoints;
-
-public static class WeatherForecastEndpoints
+namespace SmartNotes.WebApi.Endpoints
 {
-    public static IEndpointRouteBuilder MapWeatherForecastEndpoints(this IEndpointRouteBuilder routes)
+    public static class WeatherForecastEndpoints
     {
-        var group = routes.MapGroup("/weatherforecast")
-            .WithTags("weatherforecast");
+        public static IEndpointRouteBuilder MapWeatherForecastEndpoints(this IEndpointRouteBuilder routes)
+        {
+            var group = routes
+                .MapGroup("/weatherforecast")
+                .WithTags("weatherforecast");
 
-        group.MapGet("/", () =>
-            {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                        new WeatherForecast
-                        (
-                            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                            Random.Shared.Next(-20, 55),
-                            Summaries.ElementAt(Random.Shared.Next(Summaries.Count()))
-                        ))
-                    .ToArray();
-                return forecast;
-            })
-            .WithName("GetWeatherForecast");
+            group.MapGet("/", () =>
+                {
+                    var forecast = Enumerable.Range(1, 5).Select(index =>
+                            new WeatherForecast
+                            (
+                                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                                Random.Shared.Next(-20, 55),
+                                _summaries.ElementAt(Random.Shared.Next(_summaries.Count()))
+                            ))
+                        .ToArray();
+                    return forecast;
+                })
+                .WithName("GetWeatherForecast");
 
-        return routes;
-    }
+            return routes;
+        }
     
-    private static readonly IEnumerable<string> Summaries = [
-        "Freezing",
-        "Bracing",
-        "Chilly",
-        "Cool",
-        "Mild",
-        "Warm",
-        "Balmy",
-        "Hot", 
-        "Sweltering",
-        "Scorching",
-    ];
+        private static readonly IEnumerable<string> _summaries = [
+            "Freezing",
+            "Bracing",
+            "Chilly",
+            "Cool",
+            "Mild",
+            "Warm",
+            "Balmy",
+            "Hot", 
+            "Sweltering",
+            "Scorching",
+        ];
+    }
 }
--- src/SmartNotes.WebApi/Program.cs
+++ src/SmartNotes.WebApi/Program.cs
@@ -2,62 +2,71 @@ using Scalar.AspNetCore;
 using Serilog;
 using SmartNotes.WebApi.Endpoints;
 
-Log.Logger = new LoggerConfiguration()
-    .Enrich.FromLogContext()
-    .WriteTo.Console()
-    .CreateLogger();
-
-try
+namespace SmartNotes.WebApi
 {
-    var builder = WebApplication.CreateBuilder(args);
-
-    builder.Host.UseSerilog();
-
-    // Add services to the container.
-    // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-    builder.Services.AddOpenApi();
-    // Add CORS policy that allows everything
-    builder.Services.AddCors(options =>
+    public class Program
     {
-        options.AddPolicy("AllowAll", policy =>
+        public static async Task Main(string[] args)
         {
-            policy.AllowAnyOrigin()
-                .AllowAnyMethod()
-                .AllowAnyHeader();
-        });
-    });
-
-    var app = builder.Build();
-    // Logging middleware (built-in from Serilog)
-    // <-- logs method, path, status, timing, etc.
-    app.UseSerilogRequestLogging();
-    
-    // Configure the HTTP request pipeline.
-    if (builder.Configuration.GetValue<bool>("EnableApiDocumentation"))
-    {
-        app.MapOpenApi();
-        app.MapScalarApiReference("api-docs", (options) =>
-        {
-            options
-                .WithTitle("API Documentation")
-                .WithModels(false)
-                // .WithLayout(ScalarLayout.Classic)
-                .WithTheme(ScalarTheme.Mars);
-        });
+            try
+            {
+                var builder = WebApplication.CreateBuilder(args);
+                
+                builder.Logging.ClearProviders();
+                Log.Logger = new LoggerConfiguration()
+                    .Enrich.FromLogContext()
+                    .WriteTo.Console()
+                    .CreateLogger();            
+                builder.Host.UseSerilog();
+            
+                // Add services to the container.
+                // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+                builder.Services.AddOpenApi();
+                // Add CORS policy that allows everything
+                builder.Services.AddCors(options =>
+                {
+                    options.AddPolicy("AllowAll", policy =>
+                    {
+                        policy.AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader();
+                    });
+                });
+            
+                var app = builder.Build();
+                
+                // Logging middleware (built-in from Serilog)
+                // <-- logs method, path, status, timing, etc.
+                app.UseSerilogRequestLogging();
+                
+                if (builder.Configuration.GetValue<bool>("EnableApiDocumentation"))
+                {
+                    app.MapOpenApi();
+                    app.MapScalarApiReference("api-docs", (options) =>
+                    {
+                        options
+                            .WithTitle("API Documentation")
+                            .WithModels(false)
+                            // .WithLayout(ScalarLayout.Classic)
+                            .WithTheme(ScalarTheme.Mars);
+                    });
+                }
+            
+                app.UseHttpsRedirection();
+                app.UseRouting();
+                app.UseCors("AllowAll");
+                app.MapWeatherForecastEndpoints();
+            
+                await app.RunAsync();
+            }
+            catch(Exception ex)
+            {
+                Log.Logger.Error(ex, "Unexpected Error has occured on Program.cs");
+            }
+            finally
+            {
+                await Log.CloseAndFlushAsync();
+            }
+        }
     }
-
-    app.UseHttpsRedirection();
-    app.UseRouting();
-    app.UseCors("AllowAll");
-    app.MapWeatherForecastEndpoints();
-
-    await app.RunAsync();
-}
-catch(Exception ex)
-{
-    Log.Logger.Error(ex, "Unexpected Error has occured on Program.cs");
-}
-finally
-{
-    await Log.CloseAndFlushAsync();
 }
